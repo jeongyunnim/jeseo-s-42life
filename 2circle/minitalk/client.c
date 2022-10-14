dc4c78de3a8def5edec6611677d825dc6044bb49
jeongyunnim <jeongyun7521@gmail.com>
2022-10-14 10:47:26 +0900

    successed minitalk

--- 2circle/minitalk/client.c
+++ 2circle/minitalk/client.c
@@ -6,23 +6,19 @@
 /*   By: jeseo <jeseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:29:34 by jeseo             #+#    #+#             */
-/*   Updated: 2022/10/12 18:12:15 by jeseo            ###   ########.fr       */
+/*   Updated: 2022/10/13 20:45:27 by jeseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <string.h>
 #include "minitalk.h"
 
 t_info	g_info;
 
-void	signal_handler(int signo)
+void	signal_handler(int signo, siginfo_t *info, void *context)
 {
 	if (signo == SIGUSR1)
-	{
-		printf("OK\n");
 		g_info.grt_flag = 1;
-	}
 	else if (signo == SIGUSR2)
 	{
 		write(1, "wrong packet\n", 13);
@@ -30,28 +26,37 @@ void	signal_handler(int signo)
 	}
 }
 
-void	send_character(int pid, char c)
+void	send_character(int pid, unsigned char c)
 {
 	int	i;
 
 	i = 8;
-	printf("c: %c\n", c);
-	while (i-- != 0 && g_info.grt_flag == 1)
+	write(1, "-----------------\n", 18);
+	while (i != 0)
 	{
-		if (c % 2 == 1)
+		if (g_info.grt_flag == 1)
 		{
-			kill(pid, SIGUSR1);
-			printf("send 1");
+			g_info.grt_flag = 0;
+			if (c % 2 == 1)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			c /= 2;
+			i--;
 		}
-		else
-		{
-			kill(pid, SIGUSR2);
-			printf("send 2");
-		}
-		c /= 2;
-		g_info.pid_flag = 0;
-		pause();
 	}
+}
+
+int	find_server(pid_t server_pid)
+{
+	if (kill(server_pid, SIGUSR1) == -1)
+		exit(1);
+	while (1)
+	{
+		if (g_info.grt_flag == 1)
+			break ;
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -61,21 +66,17 @@ int	main(int argc, char **argv)
 
 	server_pid = atoi(argv[1]);
 	memset(&sigact, 0, sizeof(sigact));
-	g_info.grt_flag = 1;
-	sigact.__sigaction_u.__sa_handler = &signal_handler;
+	g_info.grt_flag = 0;
+	sigact.__sigaction_u.__sa_sigaction = &signal_handler;
 	sigaction(SIGUSR1, &sigact, NULL);
 	sigaction(SIGUSR2, &sigact, NULL);
+	find_server(server_pid);
 	if (argc != 3)
-	{
 		return (-1);
-	}
-	else
+	while (*argv[2])
 	{
-		while (*argv[2])
-		{
-			send_character(server_pid, *argv[2]);
-			argv[2]++;
-		}
+		send_character(server_pid, *argv[2]);
+		argv[2]++;
 	}
 	send_character(server_pid, -1);
 	write(1, "server recieved all\n", 20);
--- 2circle/minitalk/server.c
+++ 2circle/minitalk/server.c
@@ -6,72 +6,58 @@
 /*   By: jeseo <jeseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:27:20 by jeseo             #+#    #+#             */
-/*   Updated: 2022/10/12 18:51:59 by jeseo            ###   ########.fr       */
+/*   Updated: 2022/10/13 20:45:27 by jeseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <string.h>
 #include "minitalk.h"
 
 t_info	g_info;
 
-//void	signal_check(int signo, siginfo_t *info)
+//void	combine_character(char *c, char *bit_cnt)
 //{
-//	if (g_info.pid_flag == 0)
+//	if (*c != -1)
+//		write(1, c, 1);
+//	else
 //	{
-//		g_info.pid_flag = 1;
-//		if (info->si_pid != 0)
-//			g_info.pid = info->si_pid;
-//		printf("Who R U?: %d\n", info->si_pid);
-//		info->si_status
-//		g_info.grt_flag = 1;
+//		g_info.pid_flag = 0;
+//		write(1, "\n<communicatioin completed>\n", 28);
 //	}
-//	else if (g_info.pid_flag == 1 && info->si_pid != g_info.pid)
-//	{
-//		printf("client_pid: %d, current_pid: %d\n", info->si_pid, g_info.pid);
-//		printf("%d -- pid_interupt error\n", info->si_signo);
-//		if (info->si_pid != 0)
-//			kill(info->si_pid, SIGUSR2);
-//		if (g_info.pid != 0)
-//			kill(g_info.pid, SIGUSR2);
-//	}
+//	*c = 0;
+//	bit_cnt = 0;
 //}
 
 void	signal_handler(int signo, siginfo_t *info, void *context)
 {
 	static char	combine_char;
 	static char	bit_cnt;
-	static int	i;
 
-	printf("SIGUSR1: %d, signo: %d, grt_pid: %d, sig_pid: %d\n", SIGUSR1, signo, g_info.pid, info->si_pid);
-	if (signo == SIGUSR1 && g_info.grt_flag == 0)
+	if (info->si_pid == 0)
+		return ;
+	g_info.pid = info->si_pid;
+	if (g_info.pid_flag == 0)
 	{
-		g_info.grt_flag = 1;
-		g_info.pid = info->si_pid;
-		printf("communication start with %d\n", g_info.pid);
-		bit_cnt--;
+		g_info.pid_flag = 1;
+		kill(g_info.pid, SIGUSR1);
+		return ;
 	}
-	else if (signo == SIGUSR1)
-	{
+	if (signo == SIGUSR1)
 		combine_char |= (0x01 << bit_cnt);
-	}
 	bit_cnt++;
 	if (bit_cnt == 8)
 	{
 		if (combine_char != -1)
-		{
 			write(1, &combine_char, 1);
-			printf("recieved all client's massage\n");
+		else
+		{
+			g_info.pid_flag = 0;
+			write(1, "\n<communicatioin completed>\n", 28);
 		}
 		combine_char = 0;
 		bit_cnt = 0;
 	}
-	printf("bitcount:%d\n", bit_cnt);
-	if (g_info.pid != 0 && kill(g_info.pid, SIGUSR1) == -1)
-	{
-		kill(g_info.pid, SIGUSR2);
-	}
+	g_info.grt_flag = 1;
 }
 
 void	print_pid(int pid)
@@ -107,7 +93,11 @@ int	main(void)
 	sigaction(SIGUSR2, &sigact, NULL);
 	while (1)
 	{
-		pause();
+		if (g_info.grt_flag == 1)
+		{
+			g_info.grt_flag = 0;
+			kill(g_info.pid, SIGUSR1);
+		}
 	}
 	return (0);
 }
