ea94ff6911d79f36244ad89a6ab6cb681d5ee2f0
jeongyunnim <jeongyun7521@gmail.com>
2022-11-02 17:12:15 +0900

    display new window

--- 2circle/so_long/so_long.c
+++ 2circle/so_long/so_long.c
@@ -6,18 +6,19 @@
 /*   By: jeseo <jeseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 19:09:59 by jeseo             #+#    #+#             */
-/*   Updated: 2022/10/27 19:11:34 by jeseo            ###   ########.fr       */
+/*   Updated: 2022/11/02 17:07:26 by jeseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./minilibx_mms_20200219/mlx.h"
-
-
+#include "mlx.h"
 
 int	main(void)
 {
 	void	*mlx_ptr;
+	void	*win_ptr;
 
 	mlx_ptr = mlx_init();
+	mlx_new_window(mlx_ptr, 100, 100, "test");
+	mlx_loop(mlx_ptr);
 	return (0);
 }
